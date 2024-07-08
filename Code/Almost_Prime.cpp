82da7eb77ccff1b95434a44e5d07649037f20fcd
Saifahmeed <122873799+Saifahmeed@users.noreply.github.com>
2024-07-08 19:31:47 +0300

    new

--- Code/Almost_Prime.cpp
+++ Code/Almost_Prime.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
         int n;
         cin >> n;
-        int sum=1;
+        int s=1;
         for(int i=3;i<=n/2;i++){
             for(int j=2;j*j<=i;j++){
                 if(j%i==0){
@@ -12,7 +12,7 @@ int main() {
                 }
             }
         }
-        cout<<sum;
+        cout<<s;
     
     return 0;
 }
